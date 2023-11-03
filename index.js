aba2c8f868c19fc763d370ae2392d96002b5ede9
moe beydoun <moebeydoun24@yahoo.com>
2023-11-02 20:29:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14
+let num4 = 10
+const mod = num3 % num4
+
+const numbers = [5, 10, 15, 20];
+const max = Math.max(...numbers);
